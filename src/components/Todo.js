c7bf1c5daaf0790eb58fe2a1b8e41bce2d3a8e3a
Hardik <hardikganatra20@gmail.com>
2021-04-14 15:09:49 +0530

    mobile css

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,115 +5,117 @@ import { RiDeleteBin5Fill } from "react-icons/ri";
 import { motion } from "framer-motion";
 
 function Todo() {
-  const [todoList, setTodoList] = useState([]);
-  const [editFormFlag, setEditFormFlag] = useState(false);
-  const [editedTodo, seteditedTodo] = useState({
-    text: "",
-    id: null,
-    isDOne: false,
-  });
+	const [todoList, setTodoList] = useState([]);
+	const [editFormFlag, setEditFormFlag] = useState(false);
+	const [editedTodo, seteditedTodo] = useState({
+		text: "",
+		id: null,
+		isDOne: false,
+	});
 
-  const addTodo = (todoText) => {
-    let newTodo = {
-      text: todoText,
-      id: Math.floor(Math.random() * 1000000),
-      isDOne: false,
-    };
-    let addTodosToState = [...todoList, newTodo];
-    setTodoList(addTodosToState);
-  };
-  const deleteTodo = (todo) => {
-    let filteredTodoList = todoList.filter((data) => {
-      return data.id !== todo.id;
-    });
+	const addTodo = (todoText) => {
+		let newTodo = {
+			text: todoText,
+			id: Math.floor(Math.random() * 1000000),
+			isDOne: false,
+		};
+		let addTodosToState = [...todoList, newTodo];
+		setTodoList(addTodosToState);
+	};
+	const deleteTodo = (todo) => {
+		let filteredTodoList = todoList.filter((data) => {
+			return data.id !== todo.id;
+		});
 
-    setTodoList(filteredTodoList);
-  };
+		setTodoList(filteredTodoList);
+	};
 
-  const handleEdittodo = (todo) => {
-    setEditFormFlag(true);
-    seteditedTodo(todo);
-  };
+	const handleEdittodo = (todo) => {
+		setEditFormFlag(true);
+		seteditedTodo(todo);
+	};
 
-  const UpdateTodoCallback = (Editedtodo, EditedTodotext) => {
-    let UpdatedList = todoList.map((data) => {
-      if (Editedtodo.text === data.text) {
-        let upDatedTodo = data;
-        data.text = EditedTodotext;
-        return upDatedTodo;
-      } else {
-        return data;
-      }
-    });
-    setTodoList(UpdatedList);
-    setEditFormFlag(false);
-  };
+	const UpdateTodoCallback = (Editedtodo, EditedTodotext) => {
+		let UpdatedList = todoList.map((data) => {
+			if (Editedtodo.text === data.text) {
+				let upDatedTodo = data;
+				data.text = EditedTodotext;
+				return upDatedTodo;
+			} else {
+				return data;
+			}
+		});
+		setTodoList(UpdatedList);
+		setEditFormFlag(false);
+	};
 
-  const handleTodoDone = (todo) => {
-    let UpdatedList = todoList.map((data) => {
-      if (todo.id === data.id) {
-        let upDatedTodo = data;
-        data.isDOne = !data.isDOne;
-        return upDatedTodo;
-      } else {
-        return data;
-      }
-    });
-    console.log(UpdatedList);
-    setTodoList(UpdatedList);
-  };
+	const handleTodoDone = (todo) => {
+		let UpdatedList = todoList.map((data) => {
+			if (todo.id === data.id) {
+				let upDatedTodo = data;
+				data.isDOne = !data.isDOne;
+				return upDatedTodo;
+			} else {
+				return data;
+			}
+		});
+		console.log(UpdatedList);
+		setTodoList(UpdatedList);
+	};
 
-  return (
-    <motion.div animate={{ scale: 1.1 }} className="todocontainer">
-      <h3>Whats the plan Today</h3>
-      <TodoForm addTodoCallback={addTodo}></TodoForm>
-      {editFormFlag ? (
-        <TodoForm
-          name={"editedForm"}
-          UpdateTodoCallback={UpdateTodoCallback}
-          editedTodo={editedTodo}
-        ></TodoForm>
-      ) : (
-        <ul>
-          <>
-            {todoList.map((data) => {
-              return (
-                <div key={data.id}>
-                  <motion.li
-                    className={data.isDOne ? "li-done" : null}
-                    whileHover={{ scale: 1.1 }}
-                  >
-                    <motion.span
-                      onClick={() => {
-                        handleTodoDone(data);
-                      }}
-                    >
-                      {data.text}
-                    </motion.span>
+	return (
+		<motion.div animate={{ scale: 1.1 }} className="todocontainer">
+			<h3>Whats the plan Today</h3>
+			<TodoForm addTodoCallback={addTodo}></TodoForm>
+			{editFormFlag ? (
+				<TodoForm
+					name={"editedForm"}
+					UpdateTodoCallback={UpdateTodoCallback}
+					editedTodo={editedTodo}
+				></TodoForm>
+			) : (
+				<ul>
+					<>
+						{todoList.map((data) => {
+							return (
+								<div key={data.id} className="todorow">
+									<motion.li
+										className={
+											data.isDOne ? "li-done" : null
+										}
+										whileHover={{ scale: 1.1 }}
+									>
+										<motion.span
+											onClick={() => {
+												handleTodoDone(data);
+											}}
+										>
+											{data.text}
+										</motion.span>
 
-                    <div className="list-container">
-                      <GrEdit
-                        style={{ marginLeft: "10px" }}
-                        onClick={() => {
-                          handleEdittodo(data);
-                        }}
-                      ></GrEdit>
-                      <RiDeleteBin5Fill
-                        style={{ marginLeft: "10px" }}
-                        onClick={() => {
-                          deleteTodo(data);
-                        }}
-                      ></RiDeleteBin5Fill>
-                    </div>
-                  </motion.li>
-                </div>
-              );
-            })}
-          </>
-        </ul>
-      )}
-    </motion.div>
-  );
+										<div className="list-container">
+											<GrEdit
+												style={{ marginLeft: "10px" }}
+												onClick={() => {
+													handleEdittodo(data);
+												}}
+											></GrEdit>
+											<RiDeleteBin5Fill
+												style={{ marginLeft: "10px" }}
+												onClick={() => {
+													deleteTodo(data);
+												}}
+											></RiDeleteBin5Fill>
+										</div>
+									</motion.li>
+								</div>
+							);
+						})}
+					</>
+				</ul>
+			)}
+		</motion.div>
+	);
 }
 
 export default Todo;
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,39 +1,39 @@
 import React, { useState, useRef, useEffect } from "react";
 
 function TodoForm({ addTodoCallback, name, editedTodo, UpdateTodoCallback }) {
-  const [input, setInput] = useState(name ? editedTodo.text : "");
-  const Forminput = useRef(null);
-  const handleInput = (e) => {
-    setInput(e.target.value);
-  };
-  const handleSubmit = (e) => {
-    // debugger;
-    e.preventDefault();
+	const [input, setInput] = useState(name ? editedTodo.text : "");
+	const Forminput = useRef(null);
+	const handleInput = (e) => {
+		setInput(e.target.value);
+	};
+	const handleSubmit = (e) => {
+		// debugger;
+		e.preventDefault();
 
-    name ? UpdateTodoCallback(editedTodo, input) : addTodoCallback(input);
-    setInput("");
-    Forminput.current.focus();
-    console.log("formvalue", Forminput.current.focus());
-  };
+		name ? UpdateTodoCallback(editedTodo, input) : addTodoCallback(input);
+		setInput("");
+		Forminput.current.focus();
+		console.log("formvalue", Forminput.current.focus());
+	};
 
-  useEffect(() => {
-    Forminput.current.focus();
-  }, [input]);
+	useEffect(() => {
+		Forminput.current.focus();
+	}, [input]);
 
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          onChange={handleInput}
-          value={input}
-          placeholder={name ? "Edit your Todo" : "Enter your Todo"}
-          ref={Forminput}
-        ></input>
-        <button>{name ? "Edit your Todo" : "Add  your Todo"}</button>
-      </form>
-    </>
-  );
+	return (
+		<>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					onChange={handleInput}
+					value={input}
+					placeholder={name ? "Edit your Todo" : "Enter your Todo"}
+					ref={Forminput}
+				></input>
+				<button>{name ? "Edit" : "Add"}</button>
+			</form>
+		</>
+	);
 }
 
 export default TodoForm;
